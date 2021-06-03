2d31cc79ae6ad3100c608aecb7a2489b6ed0a88d
Erika Debelis <Erika.Debelis@gmail.com>
2021-06-03 11:06:58 -0700

    BUGFIX: added quotes around ladies in js
    Co-authored by: Arthur Lee <meleearthur@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,15 @@
 $(document).ready(function() {
+  $("form#quiz").submit(function(event) {
+    const option = $("input:radio[name=celebrity]:checked").val();
+
+  if (option === "ladies") {
+    $('#women').show();
+  } else if (option === "men") {
+    $('#guys').show();
+  } else {
+    $('#women').show();
+    $('#guys').show();
+  }
+  event.preventDefault();
+  });
+});
